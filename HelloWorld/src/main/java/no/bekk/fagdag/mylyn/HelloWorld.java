52f731952b97f3d358840fe346937095cbd2e5ef
Ketil Velle <ketil.velle@gmail.com>
2012-10-30 15:48:59 +0100

    Initial javacommit

---
+++ HelloWorld/src/main/java/no/bekk/fagdag/mylyn/HelloWorld.java
@@ -0,0 +1,14 @@
+package no.bekk.fagdag.mylyn;
+
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
