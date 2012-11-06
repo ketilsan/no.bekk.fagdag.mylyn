0644dab65e45f867a799c8efddd9f815b833b110
Ketil Velle <ketil.velle@gmail.com>
2012-11-06 18:56:49 +0100

    yes

--- HelloWorld/src/main/java/no/bekk/fagdag/mylyn/HelloWorld.java
+++ HelloWorld/src/main/java/no/bekk/fagdag/mylyn/HelloWorld.java
@@ -1,17 +1,8 @@
 package no.bekk.fagdag.mylyn;
 
-
 public class HelloWorld {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		System.out.println("Hei fagdag");
-		
+	public String sayHello() {
+		return "Hello World!";
 	}
-
 }
--- HelloWorld/src/test/java/TestHelloWorld.java
+++ HelloWorld/src/test/java/TestHelloWorld.java
@@ -1,13 +1,16 @@
 import static org.junit.Assert.*;
 
+import no.bekk.fagdag.mylyn.HelloWorld;
+
 import org.junit.Test;
 
 
 public class TestHelloWorld {
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void testHelloWorld() {
+		HelloWorld helloWorld = new HelloWorld();
+		assertEquals("Hello World!", helloWorld.sayHello());
 	}
 	
 	
